1f9f58dea6b19efb3d49f8f432fddd908e642822
PeterSVenton <peterstevenventon@gmail.com>
2025-08-24 20:24:55 +0100

    preparing for amplify

---
+++ src/app/(mdx-pages)/components/MdTitle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const MdTitle = (props: Props) => {
+  return (
+    <h1 className='mx-auto'>MdTitle</h1>
+  )
+}
+
+export default MdTitle
---
+++ src/app/(mdx-pages)/layout.tsx
@@ -0,0 +1,7 @@
+export default function MdxLayout({ children }: { children: React.ReactNode }) {
+    return (
+      <div className="prose mx-auto max-w-[75ch] prose-h1:text-center prose-h1:text-6xl">
+        {children}
+      </div>
+    )
+  }
