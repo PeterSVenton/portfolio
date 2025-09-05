804506873bf4f4f46b91ac4b540ff4f8ea086b59
PeterSVenton <peterstevenventon@gmail.com>
2025-09-05 21:21:43 +0100

    changed metadata on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "./sections/hero";;
 
 export const metadata = {
-  title: 'Peter Venton — BI that scales beyond dashboards',
+  title: 'Peter Venton - BI that scales beyond dashboards',
   description: 'Microsoft Fabric & Power Platform expert building scalable BI ecosystems.',
 }
 
