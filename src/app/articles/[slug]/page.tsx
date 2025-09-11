fb0333fbb4cadf80bd0ae927e775974774a40e25
PeterSVenton <peterstevenventon@gmail.com>
2025-09-11 19:51:53 +0100

    metadata now loads on md files routes

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import { articles } from "@/data/articles"
+import type { Metadata, ResolvingMetadata } from 'next'
+
 
 export default async function Page({
   params,
@@ -16,6 +18,33 @@ export function generateStaticParams() {
     // TODO: check that the slugs article exist so we don't have dead routes
   const slugs = articles.map(a => ({slug: a.slug}))
   return slugs
+}
+
+// metadata generated dynamically based on the document
+// https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}): Promise<Metadata> {
+  const { slug } = await params
+  const article = articles.find((p) => p.slug === slug)
+
+  if (!article) {
+    return {
+      title: "Not found",
+    }
+  }
+
+  return {
+    title: article.title,
+    description: article.summary,
+    openGraph: {
+      title: article.title,
+      description: article.summary,
+      url: `https://peterventon.co.uk/articles/${slug}`,
+    },
+  }
 }
  
 export const dynamicParams = false
--- src/app/work/[slug]/page.tsx
+++ src/app/work/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import { projects } from "@/data/projects"
+import type { Metadata } from 'next'
+
 
 export default async function Page({
   params,
@@ -15,6 +17,29 @@ export function generateStaticParams() {
     // TODO: check that the slugs project exist so we don't have dead routes
   const slugs = projects.map(p => ({ slug: p.slug })) 
   return slugs
+}
+
+// metadata generated dynamically based on the document
+// https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }): Promise<Metadata> {
+  const { slug } = await params
+  const project = projects.find((p) => p.slug === slug)
+
+  if (!project) {
+    return {
+      title: "Not found",
+    }
+  }
+
+  return {
+    title: project.title,
+    description: project.summary,
+    openGraph: {
+      title: project.title,
+      description: project.summary,
+      url: `https://peterventon.co.uk/projects/${slug}`,
+    },
+  }
 }
  
 export const dynamicParams = false
