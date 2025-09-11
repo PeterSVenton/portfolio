49462e166ca1f733b306fbcfc30b55436f9a8f22
PeterSVenton <peterstevenventon@gmail.com>
2025-09-11 19:38:50 +0100

    made md articles friendly for mobile

--- src/app/articles/[slug]/layout.tsx
+++ src/app/articles/[slug]/layout.tsx
@@ -1,6 +1,6 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
     return (
-      <div className="prose mx-auto max-w-[75ch] prose-h1:text-center prose-h1:text-6xl">
+      <div className="prose text-xs sm:text-sm md:text-base mx-auto px-6 sm:px-0 max-w-[75ch] prose-h1:text-center prose-h1:text-6xl py-18">
         {children}
       </div>
     )
